1974e2be527ca5a4285e03fc2d607d6a3f4e87fa
ale-drian <thalia.adrian@tecsup.edu.pe>
2021-06-16 13:39:30 -0500

    Primer Commit

---
+++ AdrianJSONRESTful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdrianJSONRESTful
+//
+//  Created by Mac 14 on 6/16/21.
+//  Copyright © 2021 Mac 14. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
